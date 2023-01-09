87d2d7cff7d269757a1e4a9a9f24b6f58f02996b
Sergey <sergeymtolstov35@gmail.com>
2023-01-09 23:47:08 +0300

    add: methods of getting Disk, RAM and CPU status

--- pkg/cloudronStatus/cloudronStatus.go
+++ pkg/cloudronStatus/cloudronStatus.go
@@ -1,18 +1,63 @@
 package cloudronStatus
 
 import (
+	"log"
+	"sync"
 	"time"
+
+	"github.com/Setom29/CloudronMonitoring/pkg/parseData"
+	"github.com/Setom29/CloudronMonitoring/pkg/requests"
 )
 
-type CloudronStatusInfo struct {
+type CloudronServer struct {
 	Domain      string
 	CpuStatus   int
 	RamStatus   int
 	DiskStatus  int
 	LastUpdated time.Time
+	cfg         parseData.Cfg
 }
 
-func NewCloudronStatusInfo(domain string) *CloudronStatusInfo {
+func NewCloudronServerConnection(cfg parseData.Cfg) *CloudronServer {
 
-	return &CloudronStatusInfo{Domain: domain, CpuStatus: 0, RamStatus: 0, DiskStatus: 0, LastUpdated: time.Now()}
+	return &CloudronServer{Domain: cfg.Domain, CpuStatus: 0, RamStatus: 0, DiskStatus: 0, LastUpdated: time.Now(), cfg: cfg}
+}
+
+func StartMonitoring(cfg parseData.Cfg, wg *sync.WaitGroup) {
+	defer wg.Done()
+	cloudronServ := NewCloudronServerConnection(cfg)
+	log.Println(cloudronServ.getDiskStatus())
+}
+
+func (cloudronServ *CloudronServer) getCpuStatus() string {
+	for {
+		res, err := requests.MakeRequest(cloudronServ.cfg.Domain + cloudronServ.cfg.DiskUrl + "?access_token=" + cloudronServ.cfg.Token)
+		if err != nil {
+			log.Println("Handling error from getCpuStatus:", err)
+		} else {
+			return res
+		}
+	}
+}
+
+func (cloudronServ *CloudronServer) getDiskStatus() string {
+	for {
+		res, err := requests.MakeRequest(cloudronServ.cfg.Domain + cloudronServ.cfg.DiskUrl + "?access_token=" + cloudronServ.cfg.Token)
+		if err != nil {
+			log.Println("Handling error from getDiskStatus:", err)
+		} else {
+			return res
+		}
+	}
+}
+
+func (cloudronServ *CloudronServer) getRamStatus() string {
+	for {
+		res, err := requests.MakeRequest(cloudronServ.cfg.Domain + cloudronServ.cfg.RamUrl + "?access_token=" + cloudronServ.cfg.Token)
+		if err != nil {
+			log.Println("Handling error from getRamStatus:", err)
+		} else {
+			return res
+		}
+	}
 }
