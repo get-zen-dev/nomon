9bd7231c5bae18b298e49ae9ae02aba6ece43da5
Setom29 <sergeymtolstov35@gmail.com>
2023-01-19 11:52:55 +0300

    add: total metrics

--- cmd/monitoringTool/main.go
+++ cmd/monitoringTool/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"log"
-
 	"github.com/Setom29/CloudronMonitoring/pkg/getServerStats"
 )
 
 func main() {
-
-	stat := getServerStats.StartMonitoring()
-	log.Printf("Cloudron stats:\n CPU Usage: %f\n Disk Usage: %f\n Memory Usage: %f\n", stat.CpuStatus, stat.DiskStatus, stat.MemStatus)
+	getServerStats.StartMonitoring()
 	// log.Println("Starting server on http://127.0.0.1:8080/")
 
 	// r := newRouter()
--- pkg/getServerStats/getServerStats.go
+++ pkg/getServerStats/getServerStats.go
@@ -11,35 +11,56 @@ import (
 )
 
 type ServerStatus struct {
-	CpuStatus   float64
-	MemStatus   float64
-	DiskStatus  float64
-	LastUpdated time.Time
-	duration    time.Duration
-	err         error
-	wg          sync.WaitGroup
+	CpuStatus        float64
+	MemStatus        float64
+	MemTotal         uint64
+	DiskStatus       float64
+	DiskTotal        uint64
+	LastUpdated      time.Time
+	duration         time.Duration
+	CpuUsageDuration time.Duration
+	err              error
+	wg               sync.WaitGroup
 }
 
 func NewCloudronServerConnection(duration uint64) *ServerStatus {
-	return &ServerStatus{CpuStatus: 0, MemStatus: 0, DiskStatus: 0, LastUpdated: time.Now(), err: nil, duration: time.Duration(duration), wg: sync.WaitGroup{}}
+
+	return &ServerStatus{
+		CpuStatus:        0,
+		MemStatus:        0,
+		MemTotal:         0,
+		DiskStatus:       0,
+		DiskTotal:        0,
+		LastUpdated:      time.Now(),
+		duration:         time.Duration(duration),
+		CpuUsageDuration: time.Duration(2),
+		err:              nil,
+		wg:               sync.WaitGroup{}}
 }
 
-func StartMonitoring() *ServerStatus {
+func StartMonitoring() {
 
 	serverStat := NewCloudronServerConnection(5)
-	serverStat.wg.Add(3)
+	serverStat.getTotalMetrics()
+	log.Printf("\nClourdon total stats:\nMemory total: %d\nDisk total: %d\n", serverStat.MemTotal, serverStat.DiskTotal)
+	for {
+		serverStat.wg.Add(3)
 
-	go serverStat.getCpu()
-	go serverStat.getMem()
-	go serverStat.getDisk()
+		go serverStat.getCpu()
+		go serverStat.getMem()
+		go serverStat.getDisk()
 
-	serverStat.wg.Wait()
-	return serverStat
+		serverStat.wg.Wait()
+		serverStat.LastUpdated = time.Now()
+		log.Printf("Cloudron stats:\n CPU Usage: %f\n Disk Usage: %f\n Memory Usage: %f\n", serverStat.CpuStatus, serverStat.DiskStatus, serverStat.MemStatus)
+		time.Sleep((serverStat.duration - serverStat.CpuUsageDuration) * time.Second)
+	}
+
 }
 
 func (serverStatus *ServerStatus) getCpu() {
 	defer serverStatus.wg.Done()
-	totalPercent, err := cpu.Percent(3*time.Second, false)
+	totalPercent, err := cpu.Percent(serverStatus.CpuUsageDuration*time.Second, false)
 	if err != nil {
 		log.Println("Error getting CPU: ", err)
 	}
@@ -63,4 +84,18 @@ func (serverStatus *ServerStatus) getDisk() {
 		log.Println("Error getting Disk: ", err)
 	}
 	serverStatus.DiskStatus = diskInfo.UsedPercent
+}
+func (serverStatus *ServerStatus) getTotalMetrics() {
+	memInfo, err := mem.VirtualMemory()
+	if err != nil {
+		log.Println("Error getting Memory: ", err)
+	}
+
+	diskInfo, err := disk.Usage("/")
+	if err != nil {
+		log.Println("Error getting Disk: ", err)
+	}
+	serverStatus.MemTotal = memInfo.Total
+
+	serverStatus.DiskTotal = diskInfo.Total
 }
